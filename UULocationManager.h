5fd6662d1ff0a484dff7d49e038703d39e76b8c3
Jonathan Hays <jon@threejacks.com>
2013-03-31 21:57:37 -0700

    Adding CLLocationManager wrapper/utility

---
+++ UULocationManager.h
@@ -0,0 +1,43 @@
+//
+//  UULocationManager.h
+//  Useful Utilities - CLLocationManager wrapper
+//
+//	License:
+//  You are free to use this code for whatever purposes you desire. The only requirement is that you smile everytime you use it.
+//
+//  Contact: @cheesemaker or jon@threejacks.com
+
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+extern NSString * const UULocationChangedNotification;
+extern NSString * const UULocationNameChangedNotification;
+extern NSString * const UULocationAuthDisabledNotification;
+extern NSString * const UULocationErrorNotification;
+
+@interface UULocationManager : NSObject<CLLocationManagerDelegate>
+
+
+//Call startTracking to begin location services. If you do not explicitly call startTracking,
+//the first time you access the sharedInstance, it will be called internally.
++ (void) startTracking;
++ (UULocationManager*) sharedInstance;
+
+
+// Cached results
+- (CLLocation*) currentLocation;
+- (NSString*)	currentLocationName;
+- (NSString*)	currentCityName;
+- (NSString*)	currentStateName;
+
+- (bool) hasValidLocation;
+
+// Configuration Properties
+@property (assign) CGFloat distanceThreshold;			//Defaults to 10 meters
+@property (assign) CGFloat timeThreshold;				//Defaults to 30 minutes (time is in seconds)
+@property (assign) bool monitorLocationName;			//Defaults to NO
+@property (assign) bool delayLocationUpdates;			//Defaults to NO
+@property (assign) NSTimeInterval locationUpdateDelay;	//Defaults to 1 second
+
+@end
---
+++ UULocationManager.m
@@ -0,0 +1,177 @@
+//
+//  UULocationManager.m
+//  Useful Utilities - CLLocationManager wrapper
+//
+//	License:
+//  You are free to use this code for whatever purposes you desire. The only requirement is that you smile everytime you use it.
+//
+//  Contact: @cheesemaker or jon@threejacks.com
+
+#import "UULocationManager.h"
+
+NSString * const UULocationChangedNotification = @"UULocationChangedNotification";
+NSString * const UULocationNameChangedNotification = @"UULocationNameChangedNotification";
+NSString * const UULocationAuthDisabledNotification = @"UULocationAuthDisabledNotification";
+NSString * const UULocationErrorNotification = @"UULocationErrorNotification";
+
+@interface UULocationManager()
+	@property (nonatomic, retain) CLLocationManager* clLocationManager;
+	@property (nonatomic, retain) CLLocation* clLocation;
+	@property (nonatomic, retain) NSString* locationName;
+    @property (nonatomic, retain) NSString* cityName;
+    @property (nonatomic, retain) NSString* stateName;
+    @property (nonatomic, retain) NSTimer* notificationTimer;
+@end
+
+static UULocationManager* theLocationManager = nil;
+
+@implementation UULocationManager
+
++ (UULocationManager*) sharedInstance
+{
+    if (theLocationManager == nil)
+    {
+        theLocationManager = [[UULocationManager alloc] init];
+    }
+    
+    return theLocationManager;
+}
+
++ (void) startTracking
+{
+	//Just call the accessor...
+	[UULocationManager sharedInstance];
+}
+
+- (CLLocation*) currentLocation
+{
+	return self.clLocation;
+}
+
+- (NSString*) currentLocationName
+{
+	return self.locationName;
+}
+
+- (NSString*) currentCityName
+{
+    return self.cityName;
+}
+
+- (NSString*) currentStateName
+{
+    return self.stateName;
+}
+
+- (id) init
+{
+	self = [super init];
+	if (self)
+	{
+        self.distanceThreshold = 10.0f;		// 10 meters
+        self.timeThreshold = 1800;			// 30 minutes
+        self.monitorLocationName = NO;
+        self.delayLocationUpdates = NO;
+        self.locationUpdateDelay = 1.0f;	// 1 second
+        
+		if ([CLLocationManager locationServicesEnabled])
+		{
+			// Initialize the locationManager
+			CLLocationManager* locManager = [[[CLLocationManager alloc] init] autorelease];        
+			locManager.delegate = self;
+            
+			locManager.desiredAccuracy = kCLLocationAccuracyBest;
+			locManager.distanceFilter = kCLDistanceFilterNone;  
+			self.clLocationManager = locManager;
+		
+			//Go!!!
+			[self.clLocationManager startUpdatingLocation];
+		}	
+	}
+	return self;
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation;
+{
+    if (!self.clLocation ||
+		!CLLocationCoordinate2DIsValid(self.clLocation.coordinate) ||
+        [self.clLocation distanceFromLocation:newLocation] > self.distanceThreshold ||
+        [self.clLocation.timestamp timeIntervalSinceDate:newLocation.timestamp] > self.timeThreshold ||
+        newLocation.horizontalAccuracy < self.clLocation.horizontalAccuracy)
+	{
+		self.clLocation = newLocation;
+
+        if (self.delayLocationUpdates)
+        {
+            [self.notificationTimer invalidate];
+            self.notificationTimer = [NSTimer scheduledTimerWithTimeInterval:self.locationUpdateDelay target:self selector:@selector(postLocationChangedTimer:) userInfo:self.clLocation repeats:NO];
+        }
+        else
+        {
+            [[NSNotificationCenter defaultCenter] postNotificationName:UULocationChangedNotification object:self.clLocation];
+        }
+	
+        if (self.monitorLocationName)
+        {
+            [self queryLocationName:self.clLocation];
+        }
+	}
+}
+
+- (void) postLocationChangedTimer:(NSTimer*)timer
+{
+    CLLocation* location = timer.userInfo;
+    [[NSNotificationCenter defaultCenter] postNotificationName:UULocationChangedNotification object:location];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:UULocationErrorNotification object:error];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    if (status == kCLAuthorizationStatusDenied)
+    {
+        [[NSNotificationCenter defaultCenter] postNotificationName:UULocationAuthDisabledNotification object:nil];
+    }
+}
+
+- (void) queryLocationName:(CLLocation*)location
+{
+    CLGeocoder* geoCoder = [[[CLGeocoder alloc] init] autorelease];
+    [geoCoder reverseGeocodeLocation:location completionHandler:^(NSArray* placemarks, NSError* error)
+     {
+         if (error == nil && placemarks != nil && placemarks.count > 0)
+         {
+             CLPlacemark* info = [placemarks objectAtIndex:0];
+             
+             NSMutableString* sb = [NSMutableString string];
+             if (info.locality != nil)
+             {
+                 [sb appendString:info.locality];
+                 self.cityName = info.locality;
+             }
+             
+             if (info.administrativeArea != nil)
+             {
+                 if (sb.length > 0)
+                 {
+                     [sb appendString:@", "];
+                 }
+                 [sb appendString:info.administrativeArea];
+                 self.stateName = info.administrativeArea;
+             }
+             
+             self.locationName = sb;
+             [[NSNotificationCenter defaultCenter] postNotificationName:UULocationNameChangedNotification object:sb];
+         }
+     }];
+}
+
+- (bool) hasValidLocation
+{
+    return ((self.clLocation != nil) && CLLocationCoordinate2DIsValid(self.clLocation.coordinate));
+}
+
+@end
