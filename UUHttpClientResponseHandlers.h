88cd1b471eebef448cbd098673e4a89ac396a36e
Ryan DeVore <ryan@threejacks.com>
2013-07-21 09:30:24 -0700

    Adding UUHttpClient - A lightweight Objective-C Http Client

---
+++ UUHttpClientResponseHandlers.h
@@ -0,0 +1,29 @@
+//
+//  UUHttpClientResponseHandlers.h
+//  Useful Utilities - Commonly used Response Handlers for UUHttpClient
+//
+//	License:
+//  You are free to use this code for whatever purposes you desire. The only requirement is that you smile everytime you use it.
+//
+//  Contact: @cheesemaker or jon@threejacks.com
+
+
+#import <Foundation/Foundation.h>
+#import "UUHttpClient.h"
+
+extern NSString * const kUUContentTypeApplicationJson;
+extern NSString * const kUUContentTypeTextJson;
+extern NSString * const kUUContentTypeTextHtml;
+extern NSString * const kUUContentTypeBinary;
+
+@interface UUTextResponseHandler : NSObject<UUHttpResponseHandler>
+
+@end
+
+@interface UUBinaryResponseHandler : NSObject<UUHttpResponseHandler>
+
+@end
+
+@interface UUJsonResponseHandler : NSObject<UUHttpResponseHandler>
+
+@end
