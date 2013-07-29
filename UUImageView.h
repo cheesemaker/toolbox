43dd3cf370cd70273652328bdf7d84a9eac80574
Ryan DeVore <ryan@threejacks.com>
2013-07-29 14:35:09 -0700

    UIImageView category for remotely loading images

---
+++ UUImageView.h
@@ -0,0 +1,18 @@
+//
+//  UIImageView.h
+//  Useful Utilities - UIImageView extensions
+//
+//	License:
+//  You are free to use this code for whatever purposes you desire. The only requirement is that you smile everytime you use it.
+//
+//  Contact: @cheesemaker or jon@threejacks.com
+
+#import <Foundation/Foundation.h>
+
+@interface UIImageView (UURemoteLoading)
+
+- (void) uuLoadImageFromURL:(NSURL*)url
+               defaultImage:(UIImage*)defaultImage
+        loadCompleteHandler:(void (^)(UIImageView* imageView))loadCompleteHandler;
+
+@end
