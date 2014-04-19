33f8fbaf8e366d97cff9f90335a40aa54f2b5f15
Ryan DeVore <ryan@threejacks.com>
2014-04-18 23:50:58 -0700

    Added unit test project for UU core files

---
+++ Test/UUFrameworkTest/UUToolboxTest/UUToolboxTest.m
@@ -0,0 +1,34 @@
+//
+//  UUToolboxTest.m
+//  UUToolboxTest
+//
+//  Created by Ryan DeVore on 4/18/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UUToolboxTest : XCTestCase
+
+@end
+
+@implementation UUToolboxTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
