bfb29346a3eaeb55660e9616e12fe5df1e36eb77
Ryan DeVore <ryan@threejacks.com>
2014-04-28 22:54:11 -0700

    Added UIView resizing and positioning categories

--- UUView.h
+++ UUView.h
@@ -29,3 +29,69 @@
 - (void) uuAnimateFadeDisappear:(BOOL)removeFromSuperview;
 
 @end
+
+@interface UIView (UUViewPositioning)
+
+// The original frame of this view, as defined in either a XIB or an initWithFrame
+- (CGRect) uuOriginalFrame;
+
+// Positioning helpers to move the frame relative to an anchor view
+- (void) uuPositionRightOf:(UIView*)anchorView withSpacing:(CGFloat)spacing;
+- (void) uuPositionLeftOf:(UIView*)anchorView withSpacing:(CGFloat)spacing;
+- (void) uuPositionBelow:(UIView*)anchorView withSpacing:(CGFloat)spacing;
+- (void) uuPositionAbove:(UIView*)anchorView withSpacing:(CGFloat)spacing;
+
+// Alignment helpers to move the frame to align with an anchor view
+- (void) uuAlignLeft:(UIView*)anchorView margin:(CGFloat)margin;
+- (void) uuAlignRight:(UIView*)anchorView margin:(CGFloat)margin;
+- (void) uuAlignTop:(UIView*)anchorView margin:(CGFloat)margin;
+- (void) uuAlignBottom:(UIView*)anchorView margin:(CGFloat)margin;
+
+// Convenience alignment methods
+- (void) uuAlignToParentLeft:(CGFloat)margin;
+- (void) uuAlignToParentRight:(CGFloat)margin;
+- (void) uuAlignToParentBottom:(CGFloat)margin;
+- (void) uuAlignToParentTop:(CGFloat)margin;
+
+// Centering methods
+- (void) uuCenterHorizontallyInParent;
+- (void) uuCenterVerticallyInParent;
+- (void) uuCenterInParent;
+
+@end
+
+@interface UIView (UUViewResizing)
+
+// Resizes only a view's width to fit its contents
+- (void) uuResizeWidth;
+
+// Resizes width with a minimum constraint. Passing a negative value will not
+// enforce a minimum
+- (void) uuResizeWidth:(CGFloat)minimumWidth;
+
+// Convenience method to resize the width while passing the original height as
+// the minimum constraint.
+- (void) uuResizeWidthOriginalAsMin;
+
+// Resizes only a view's height to fit its contents
+- (void) uuResizeHeight;
+
+// Resizes height with a minimum constraint. Passing a negative value will not
+// enforce a minimum
+- (void) uuResizeHeight:(CGFloat)minimumHeight;
+
+// Convenience method to resize the height while passing the original height as
+// the minimum constraint.
+- (void) uuResizeHeightOriginalAsMin;
+
+// Resizes both width and height.  This is equivalent to sizeToFit with the
+// exception that the original frame is restored prior to calling sizeToFit.
+// This is useful when dynamically resizing text views inside of table cells
+// where the text view frame might shrink from one cell to the next.
+- (void) uuResizeWidthAndHeight;
+
+// Resizes both width and height with a fixed amount of padding and optional
+// minimum sizes
+- (void) uuResizeWidthAndHeight:(UIEdgeInsets)padding minSize:(CGSize)minSize;
+
+@end
