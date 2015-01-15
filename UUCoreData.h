031f7ba61280b888357c3d89a383e14e51ae5739
Ryan DeVore <ryan@threejacks.com>
2015-01-14 16:02:05 -0800

    Stubbed UUCoreData file

---
+++ UUCoreData.h
@@ -0,0 +1,16 @@
+//
+//  UUCoreData.h
+//  Useful Utilities - CoreData helpers
+//
+//	License:
+//  You are free to use this code for whatever purposes you desire. The only requirement is that you smile everytime you use it.
+//
+//  Contact: @cheesemaker or jon@threejacks.com
+//
+
+@import Foundation;
+@import CoreData;
+
+@interface UUCoreData : NSObject
+
+@end
