be93237baf2ead59b0a1b4796c9bcf75b4a588e4
Ryan DeVore <ryan@threejacks.com>
2015-01-14 21:59:14 -0800

    Initial implementation of basic core data helpers

--- UUCoreData.h
+++ UUCoreData.h
@@ -11,6 +11,59 @@
 @import Foundation;
 @import CoreData;
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// UUCOreData
+
 @interface UUCoreData : NSObject
+
+// Setup the Core Data store. This MUST be called prior to using any other UUCoreData methods
++ (void) configureWithStoreUrl:(NSURL*)storeUrl modelDefinitionBundle:(NSBundle*)bundle;
+
+// Singleton managed object context shared by entire application
++ (NSManagedObjectContext*) mainThreadContext;
+
+// Short lived managed object context for use on background threads
++ (NSManagedObjectContext*) workerThreadContext;
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// NSManagedObjectContext extensions
+
+@interface NSManagedObjectContext (UUCoreData)
+
+- (BOOL) uuSubmitChanges;
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// NSManagedObject extensions
+
+@interface NSManagedObject (UUCoreData)
+
++ (NSString*) uuEntityName;
+
++ (NSFetchRequest*) uuFetchRequestInContext:(NSManagedObjectContext*)context
+                                  predicate:(NSPredicate*)predicate
+                            sortDescriptors:(NSArray*)sortDescriptors
+                                      limit:(NSNumber*)limit;
+
++ (NSArray*) uuFetchObjectsWithPredicate:(NSPredicate*)predicate
+                                 context:(NSManagedObjectContext*)context;
+
++ (NSArray*) uuFetchObjectsWithPredicate:(NSPredicate*)predicate
+                         sortDescriptors:(NSArray*)sortDescriptors
+                                   limit:(NSNumber*)limit
+                                 context:(NSManagedObjectContext*)context;
+
++ (instancetype) uuFetchSingleObjectWithPredicate:(NSPredicate*)predicate
+                                          context:(NSManagedObjectContext*)context;
+
++ (instancetype) uuFetchSingleObjectWithPredicate:(NSPredicate*)predicate
+                                  sortDescriptors:(NSArray*)sortDescriptors
+                                          context:(NSManagedObjectContext*)context;
+
++ (instancetype) uuFetchOrCreateSingleEntityWithPredicate:(NSPredicate *)predicate
+                                                  context:(NSManagedObjectContext *)context;
 
 @end
--- UUCoreData.m
+++ UUCoreData.m
@@ -10,6 +10,255 @@
 
 #import "UUCoreData.h"
 
+//If you want to provide your own logging mechanism, define UUDebugLog in your .pch
+#ifndef UUDebugLog
+#ifdef DEBUG
+#define UUDebugLog(fmt, ...) NSLog(fmt, ##__VA_ARGS__)
+#else
+#define UUDebugLog(fmt, ...)
+#endif
+#endif
+
+#pragma mark - NSError Private Extensions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// UUCoreData
+
+@interface NSError (UUCoreData)
+
+- (void) uuLogDetailedErrors;
+
+@end
+
+#pragma mark - UUCoreData
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// UUCOreData
+
+@interface UUCoreData ()
+
+@property (nonatomic, strong) NSPersistentStoreCoordinator* storeCoordinator;
+@property (nonatomic, strong) NSManagedObjectContext* mainThreadContext;
+
+@end
+
 @implementation UUCoreData
+
++ (void) configureWithStoreUrl:(NSURL*)storeUrl modelDefinitionBundle:(NSBundle*)bundle
+{
+    [[self sharedInstance] configureWithStoreUrl:storeUrl modelDefinitionBundle:bundle];
+}
+
++ (instancetype) sharedInstance
+{
+    static id theSharedObject = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once (&onceToken, ^
+    {
+        theSharedObject = [[[self class] alloc] init];
+    });
+    
+    return theSharedObject;
+}
+
+- (void) configureWithStoreUrl:(NSURL*)storeUrl modelDefinitionBundle:(NSBundle*)bundle
+{
+    if (!bundle)
+    {
+        bundle = [NSBundle mainBundle];
+    }
+    
+    NSManagedObjectModel* managedObjectModel = [NSManagedObjectModel mergedModelFromBundles:@[bundle]];
+    
+    NSError* error;
+    
+    NSDictionary* options =
+    @{
+      NSMigratePersistentStoresAutomaticallyOption : @(YES),
+      NSInferMappingModelAutomaticallyOption : @(YES)
+      };
+    
+    self.storeCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:managedObjectModel];
+    if (![self.storeCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeUrl options:options error:&error])
+    {
+        UUDebugLog(@"Cannot Setup Core Data!");
+        UUDebugLog(@"Error: %@", [error description]);
+        exit(-1);
+    }
+    
+    self.mainThreadContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+    [self.mainThreadContext setPersistentStoreCoordinator:self.storeCoordinator];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(otherContextDidSave:) name:NSManagedObjectContextDidSaveNotification object:nil];
+}
+
++ (NSManagedObjectContext*) mainThreadContext
+{
+    return [[self sharedInstance] mainThreadContext];
+}
+
+- (void)otherContextDidSave:(NSNotification *)didSaveNotification
+{
+    NSManagedObjectContext* context = (NSManagedObjectContext *)didSaveNotification.object;
+    
+    NSManagedObjectContext* destContext = self.mainThreadContext;
+    if (context != destContext)
+    {
+        [destContext performBlock:^
+         {
+             UUDebugLog(@"Merging changes from background context");
+             [destContext mergeChangesFromContextDidSaveNotification:didSaveNotification];
+         }];
+    }
+}
+
++ (NSManagedObjectContext*) workerThreadContext
+{
+    NSPersistentStoreCoordinator* coordinator = [[self sharedInstance] storeCoordinator];
+    NSManagedObjectContext* context = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
+    [context setPersistentStoreCoordinator:coordinator];
+    return context;
+}
+
+@end
+
+#pragma mark - NSManagedObjectContext Extensions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// NSManagedObjectContext extensions
+
+@implementation NSManagedObjectContext (UUCoreData)
+
+- (BOOL) uuSubmitChanges
+{
+    NSError* error = nil;
+    if ([self hasChanges])
+    {
+        BOOL result = [self save:&error];
+        
+        if (!result)
+        {
+            [error uuLogDetailedErrors];
+            return NO;
+        }
+    }
+    
+    return YES;
+}
+
+@end
+
+#pragma mark - NSManagedObject Extensions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// NSManagedObject extensions
+
+@implementation NSManagedObject (UUCoreData)
+
++ (NSString*) uuEntityName
+{
+    return NSStringFromClass([self class]);
+}
+
++ (NSFetchRequest*) uuFetchRequestInContext:(NSManagedObjectContext*)context
+                                  predicate:(NSPredicate*)predicate
+                            sortDescriptors:(NSArray*)sortDescriptors
+                                      limit:(NSNumber*)limit
+{
+    NSFetchRequest* fetchRequest = [[NSFetchRequest alloc] init];
+    fetchRequest.entity = [NSEntityDescription entityForName:[self uuEntityName] inManagedObjectContext:context];
+    fetchRequest.sortDescriptors = sortDescriptors;
+    fetchRequest.predicate = predicate;
+    
+    if (limit != nil)
+    {
+        [fetchRequest setFetchLimit:[limit unsignedIntegerValue]];
+    }
+    
+    return fetchRequest;
+}
+
++ (NSArray*) uuFetchObjectsWithPredicate:(NSPredicate*)predicate
+                                 context:(NSManagedObjectContext*)context
+{
+    return [self uuFetchObjectsWithPredicate:predicate sortDescriptors:nil limit:nil context:context];
+}
+
++ (NSArray*) uuFetchObjectsWithPredicate:(NSPredicate*)predicate
+                         sortDescriptors:(NSArray*)sortDescriptors
+                                   limit:(NSNumber*)limit
+                                 context:(NSManagedObjectContext*)context
+{
+    NSError* err = nil;
+    
+    NSFetchRequest* fetchRequest = [self uuFetchRequestInContext:context predicate:predicate sortDescriptors:sortDescriptors limit:limit];
+    
+    NSArray* fetchResults = [context executeFetchRequest:fetchRequest error:&err];
+    if (err)
+    {
+        [err uuLogDetailedErrors];
+    }
+    
+    return fetchResults;
+}
+
++ (instancetype) uuFetchSingleObjectWithPredicate:(NSPredicate*)predicate
+                                          context:(NSManagedObjectContext*)context
+{
+    return [self uuFetchSingleObjectWithPredicate:predicate sortDescriptors:nil context:context];
+}
+
++ (instancetype) uuFetchSingleObjectWithPredicate:(NSPredicate*)predicate
+                                  sortDescriptors:(NSArray*)sortDescriptors
+                                          context:(NSManagedObjectContext*)context
+{
+    NSArray* list = [self uuFetchObjectsWithPredicate:predicate sortDescriptors:sortDescriptors limit:@(1) context:context];
+    if (list && list.count > 0)
+    {
+        return list[0];
+    }
+    else
+    {
+        return nil;
+    }
+}
+
++ (instancetype) uuFetchOrCreateSingleEntityWithPredicate:(NSPredicate *)predicate context:(NSManagedObjectContext *)context
+{
+    NSManagedObject* obj = [self uuFetchSingleObjectWithPredicate:predicate context:context];
+    
+    if (!obj)
+    {
+        obj = [NSEntityDescription insertNewObjectForEntityForName:[self uuEntityName] inManagedObjectContext:context];
+    }
+    
+    return obj;
+}
+
+@end
+
+
+
+#pragma mark - NSError Extensions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// NSError extensions
+
+@implementation NSError (UUCoreData)
+
+- (void) uuLogDetailedErrors
+{
+    NSError* error = self;
+    
+    UUDebugLog(@"ERROR: %@", [error localizedDescription]);
+    NSArray* detailedErrors = [[error userInfo] objectForKey:NSDetailedErrorsKey];
+    if (detailedErrors != nil && [detailedErrors count] > 0)
+    {
+        for (NSError* de in detailedErrors)
+        {
+            UUDebugLog(@"  DetailedError: %@", [de userInfo]);
+        }
+    }
+    else
+    {
+        UUDebugLog(@"  %@", [error userInfo]);
+    }
+}
 
 @end
