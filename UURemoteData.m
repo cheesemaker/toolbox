b148c4faadd739026d3037bae104a7e611fb8c7c
Ryan DeVore <ryan@threejacks.com>
2015-02-07 22:22:34 -1000

    Switch UURemoteData to use new UUHttpSession class

--- UURemoteData.m
+++ UURemoteData.m
@@ -8,7 +8,7 @@
 //
 
 #import "UURemoteData.h"
-#import "UUHttpClient.h"
+#import "UUHttpSession.h"
 #import "UUDataCache.h"
 
 //If you want to provide your own logging mechanism, define UUDebugLog in your .pch
@@ -29,6 +29,7 @@ NSString * const kUUErrorKey                        = @"UUErrorKey";
 @interface UURemoteData ()
 
 @property (nonatomic, strong) NSMutableDictionary* pendingDownloads;
+
 @end
 
 @implementation UURemoteData
@@ -71,7 +72,7 @@ NSString * const kUUErrorKey                        = @"UUErrorKey";
         return data;
     }
     
-    UUHttpClient* pendingDownload = [self.pendingDownloads valueForKey:path];
+    UUHttpRequest* pendingDownload = [self.pendingDownloads valueForKey:path];
     if (pendingDownload)
     {
         // An active UUHttpClient means a request is currently fetching the resource, so
@@ -80,9 +81,9 @@ NSString * const kUUErrorKey                        = @"UUErrorKey";
         return nil;
     }
     
-    UUHttpClientRequest* request = [UUHttpClientRequest getRequest:path queryArguments:nil];
+    UUHttpRequest* request = [UUHttpRequest getRequest:path queryArguments:nil];
 	request.processMimeTypes = NO;
-    UUHttpClient* client = [UUHttpClient executeRequest:request completionHandler:^(UUHttpClientResponse *response)
+    UUHttpRequest* client = [UUHttpSession executeRequest:request completionHandler:^(UUHttpResponse *response)
     {
         [self handleDownloadResponse:response forPath:path];
     }];
@@ -92,7 +93,7 @@ NSString * const kUUErrorKey                        = @"UUErrorKey";
     return nil;
 }
 
-- (void) handleDownloadResponse:(UUHttpClientResponse*)response forPath:(NSString*)path
+- (void) handleDownloadResponse:(UUHttpResponse*)response forPath:(NSString*)path
 {
     NSMutableDictionary* md = [NSMutableDictionary dictionary];
     [md setValue:path forKeyPath:kUUDataRemotePathKey];
@@ -102,14 +103,23 @@ NSString * const kUUErrorKey                        = @"UUErrorKey";
         [[UUDataCache sharedCache] setObject:response.rawResponse forKey:path];
         
         [md setValue:response.rawResponse forKeyPath:kUUDataKey];
-        [[NSNotificationCenter defaultCenter] postNotificationName:kUUDataDownloadedNotification object:nil userInfo:md];
+        
+        dispatch_async(dispatch_get_main_queue(), ^
+        {
+            [[NSNotificationCenter defaultCenter] postNotificationName:kUUDataDownloadedNotification object:nil userInfo:md];
+        });
+        
     }
     else
     {
         UUDebugLog(@"Image Download Failed!\n\nPath: %@\nStatusCode: %d\nError: %@\n", path, (int)response.httpResponse.statusCode, response.httpError);
         
         [md setValue:response.httpError forKeyPath:kUUErrorKey];
-        [[NSNotificationCenter defaultCenter] postNotificationName:kUUDataDownloadFailedNotification object:nil userInfo:md];
+        
+        dispatch_async(dispatch_get_main_queue(), ^
+        {
+            [[NSNotificationCenter defaultCenter] postNotificationName:kUUDataDownloadFailedNotification object:nil userInfo:md];
+        });
     }
     
     [self.pendingDownloads removeObjectForKey:path];
@@ -121,7 +131,7 @@ NSString * const kUUErrorKey                        = @"UUErrorKey";
     __block int totalToProcess = (int)remotePaths.count;
     __block NSMutableDictionary* md = [NSMutableDictionary dictionary];
     
-    void (^block)(NSString* remotePath, UUHttpClientResponse* response) = ^(NSString* remotePath, UUHttpClientResponse* response)
+    void (^block)(NSString* remotePath, UUHttpResponse* response) = ^(NSString* remotePath, UUHttpResponse* response)
     {
         ++processedCount;
         
@@ -143,9 +153,9 @@ NSString * const kUUErrorKey                        = @"UUErrorKey";
     
     for (NSString* path in remotePaths)
     {
-        UUHttpClientRequest* request = [UUHttpClientRequest getRequest:path queryArguments:nil];
+        UUHttpRequest* request = [UUHttpRequest getRequest:path queryArguments:nil];
         request.processMimeTypes = NO;
-        UUHttpClient* client = [UUHttpClient executeRequest:request completionHandler:^(UUHttpClientResponse *response)
+        UUHttpRequest* client = [UUHttpSession executeRequest:request completionHandler:^(UUHttpResponse *response)
         {
             [self handleDownloadResponse:response forPath:path];
             block(path, response);
