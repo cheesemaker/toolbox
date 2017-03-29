8077a53e516f61833f980771d4140bdeac8c24d7
Ryan DeVore <ryan@silverpinesoftware.com>
2017-03-29 14:20:11 -0700

    Swift debug logging!

---
+++ swift/UUDebugLog.swift
@@ -0,0 +1,42 @@
+//
+//  UUDebugLog.swift
+//  Useful Utilities - NSLog wrapper that only prints in debug mode
+//
+//  Created by Ryan DeVore on 03/29/2017
+//
+//	License:
+//  You are free to use this code for whatever purposes you desire.
+//  The only requirement is that you smile everytime you use it.
+//
+//  Contact: @ryandevore or ryan@silverpine.com
+//
+
+import Foundation
+
+// 
+// UUDebugLog appends file, method, and line information to your custom logging
+// message and prints to NSLog only when the preprocessor symbol DEBUG is defined
+//
+// In Swift projects, you can define a symbol in the 'Other Swift Flags' as
+// "-D DEBUG" (without the quotes)
+//
+// Usage is identical to traditional Objective-C NSLog,
+//
+// UUDebugLog("Some interesting log statement")
+//
+// or with paramaters:
+//
+// UUDebugLog("Another interesting log statement, foo: %@, bar: %@", "Foo", "Bar")
+//
+func UUDebugLog(function : NSString = #function, file : NSString = #file, line : Int = #line, _ format : String, _ args: CVarArg...)
+{
+#if DEBUG
+    withVaList(args,
+    { (p : CVaListPointer) -> Void in
+ 
+        let fileNameOnly : String = file.lastPathComponent
+        let s = NSString.init(format: "\(fileNameOnly) [\(function):\(line)] - \(format)", arguments: p)
+        NSLog(s as String)
+    })
+#endif
+}
